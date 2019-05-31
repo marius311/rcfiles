86063bf66ab881de9b4b14f33ea16a8a9b25bf1d
marius <mariusmillea@gmail.com>
2019-05-31 12:01:50 -0700

    fix install for gitconfig

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for f in $(find . ! -name install.sh ! -path "./.git*" -type f); do
+for f in $(find . ! -name install.sh ! -path "./.git/*" -type f); do
     ln -ivs $(readlink -f $f) $(readlink -f $HOME/$f)
 done
